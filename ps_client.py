b4b227c173e2f28c5dd23c3540729715807f7609
Piotr Machura <piotr.machura16@gmail.com>
2021-05-29 17:58:08 +0200

    Remote server deployed

--- ps_client.py
+++ ps_client.py
@@ -1,6 +1,5 @@
 """Client executable module."""
 
-import time
 import socket
 
 from common import request
@@ -8,7 +7,7 @@ from common import response
 from common.message import MessageType
 from common import transfer
 
-HOST = 'localhost'    # The server's hostname or IP address
+HOST = 'piotr-machura.com'    # The server's hostname or IP address
 PORT = 9999    # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -25,10 +24,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     myID = resp.sessionID
     # Request files owned and borrowed by the user
     print(myID)
-    req = request.NewFileRequest(fileName='a.txt')
-    transfer.send(s, req)
-    resp = response.OkResponse.fromJSON(transfer.recieve(s))
-    print(resp)
+    # req = request.NewFileRequest(fileName='a.txt')
+    # transfer.send(s, req)
+    # resp = response.OkResponse.fromJSON(transfer.recieve(s))
+    # print(resp)
     # req = request.NewFileRequest(fileName='b.txt')
     # transfer.send(s, req)
     # resp = response.OkResponse.fromJSON(transfer.recieve(s))
@@ -42,18 +41,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         exit(1)
     for fileID, data in resp.files.items():
         print(data)
-        if data['name'] == 'a.txt':
-            req = request.DeleteShareRequest(fileID=fileID, user='alice')
-            transfer.send(s, req)
-            resp = response.OkResponse.fromJSON(transfer.recieve(s))
-            print(resp)
-
-        # if data['name'] == 'b.txt':
-        #     req = request.PushRequest(fileID=fileID, content='NEW BFILE 4')
+        # if data['name'] == 'a.txt':
+        #     req = request.DeleteShareRequest(fileID=fileID, user='alice')
         #     transfer.send(s, req)
         #     resp = response.OkResponse.fromJSON(transfer.recieve(s))
         #     print(resp)
-        #     req = request.PullRequest(fileID=fileID)
-        #     transfer.send(s, req)
-        #     resp = response.PullResponse.fromJSON(transfer.recieve(s))
-        #     print(resp.content)
+
+        if data['name'] == 'b.txt':
+            req = request.PullRequest(fileID=fileID)
+            transfer.send(s, req)
+            resp = response.PullResponse.fromJSON(transfer.recieve(s))
+            print(resp.content)
+            req = request.PushRequest(fileID=fileID, content='NEW BFILE BY BILLY')
+            transfer.send(s, req)
+            resp = response.OkResponse.fromJSON(transfer.recieve(s))
+            print(resp)
+            req = request.PullRequest(fileID=fileID)
+            transfer.send(s, req)
+            resp = response.PullResponse.fromJSON(transfer.recieve(s))
+            print(resp.content)
